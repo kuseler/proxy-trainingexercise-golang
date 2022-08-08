50dc12cff1681472412f7d5ea0aabef26a4ee36f
kuseler <47039588+kuseler@users.noreply.github.com>
2022-08-08 11:05:51 +0200

    updated proxy-trainingexercise-golang.go
    it now prints out those request lines that didn't produce an error

--- proxy-trainingexercise-golang.go
+++ proxy-trainingexercise-golang.go
@@ -5,23 +5,64 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 )
 
 /*
 https://pkg.go.dev/net will probably do the most work for me,
 user makes requests to url, the proxy forwards them to the url, including headers and
-status codes.*/
+status codes.
+
+*/
 
 //https://www.upguard.com/blog/proxy-server
 
 //my time limit is 2 days, but I hope I'll be faster than that
 
-func main() {
-	conn, err := net.Dial("tcp", "golang.org:80")
-	if err != nil {
-		// handle error
+//TODO: improve Error Handling
+
+func getHeaders(r http.Request) bool {
+	// Loop over header names
+	for name, values := range r.Header {
+		// Loop over all values for the name.
+		for _, value := range values {
+			fmt.Println(name, value)
+		}
 	}
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
-	status, err := bufio.NewReader(conn).ReadString('\n')
-	fmt.Println(status, err)
+	return true
+}
+
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	bufReader := bufio.NewReader(c)
+
+	for {
+		// Read tokens delimited by newline
+		bytes, err := bufReader.ReadBytes('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("%s", bytes)
+	}
+}
+
+func listen(port string) {
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println("an error occured:", err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("an error occured:", err)
+		}
+		go handleConnection(conn)
+	}
+
+}
+
+func main() {
+	listen(":8888")
 }
