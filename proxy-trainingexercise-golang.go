d1d2e1262701ff37c25f4435895860d37fb0237b
kuseler <47039588+kuseler@users.noreply.github.com>
2022-08-09 09:16:52 +0200

    Add files via upload

--- proxy-trainingexercise-golang.go
+++ proxy-trainingexercise-golang.go
@@ -12,33 +12,36 @@ import (
 )
 
 /*
-https://pkg.go.dev/net will probably do the most Fetchwork for me.
+https://pkg.go.dev/net will probably do the most work for me.
 user makes requests to url, the proxy forwards them to the url, including headers and
 status codes.
 */
 
 //https://www.upguard.com/blog/proxy-server
-//I have 1 day left, but I hope I'll be faster than that
+//I have 1 day left
 //TODO: improve Error Handling
 //TODO improve readability
+//weird bug where the page has to be reloaded in order for the program to do anything
 
-func handleConnection(c net.Conn) {
-	defer fmt.Println("closing")
-	defer c.Close()
+func getHeaders(connection net.Conn) []string {
 	var headers []string
-	bufReader := bufio.NewReader(c)
+	bufReader := bufio.NewReader(connection)
 	for {
 		// Read tokens delimited by newline
 		bytes, err := bufReader.ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
 			if err == io.EOF {
 				fmt.Println("EOF reached!")
 				break
 			}
+			fmt.Println(err)
 		}
 		headers = append(headers, string(bytes))
 	}
+	return headers
+}
+
+func genRequest(headers []string) *http.Request {
 	method := strings.Split(headers[0], " ")[0]
 	url := strings.Split(headers[0], " ")[1][1:]
 	req, err := http.NewRequest(method, url, nil)
@@ -52,7 +55,16 @@ func handleConnection(c net.Conn) {
 			req.Header.Set(strings.TrimSpace(line[0]), strings.TrimSpace(line[1]))
 		}
 	}
-	res, err := http.DefaultClient.Do(req)
+	return req
+}
+
+func handleConnection(c net.Conn) {
+	defer fmt.Println("closing")
+	defer c.Close()
+	headers := getHeaders(c)
+	// here is the actual request
+	request := genRequest(headers)
+	res, err := http.DefaultClient.Do(request)
 	if err != nil {
 		fmt.Println("error while making Request", err)
 	}
@@ -61,7 +73,7 @@ func handleConnection(c net.Conn) {
 	if err != nil {
 		fmt.Println("error trying to read response body", err)
 	}
-	fmt.Printf("client: response body: %s\n", resBody)
+	fmt.Println(string(resBody))
 }
 
 func listen(port string) {
